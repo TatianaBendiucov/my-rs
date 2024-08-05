11ed9f6d196d84cfe649fb6f0d5b7b893174b0d6
Tanya G <guriev.tatiana@mail.ru>
2024-08-05 14:54:27 +0300

    jest config update

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,14 @@ const config: Config.InitialOptions = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: ["next/babel"],
+      },
+    ],
+  },
 };
 
 export default config;

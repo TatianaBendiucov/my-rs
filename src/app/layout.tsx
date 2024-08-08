312f5777ea7573d9868b9c522d7dddfaed224a47
Tanya G <guriev.tatiana@mail.ru>
2024-08-08 14:26:33 +0300

    app route layout and pages

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { ThemeProvider } from "../context/ThemeContext";
+import ErrorBoundary from "../components/ErrorBoundary";
+import "../index.css";
+import { ReduxProvider } from "src/store/reduxProvider";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html>
+      <body>
+        <ThemeProvider>
+          <ReduxProvider>
+            <ErrorBoundary
+              fallback={<p>ErrorBoundary: Something went wrong.</p>}
+            >
+              {children}
+            </ErrorBoundary>
+          </ReduxProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
